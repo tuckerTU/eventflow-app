275d3d769365f6899fd6e91cc5ed6cdc9cb09e53
tuckerTU <tuckerishere@gmail.com>
2021-07-19 15:06:54 -0600

    work on users controller/sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+  get '/signup', to: 'users#new'
+
+
   resources :users
   resources :hosts
   resources :guests
   resources :events
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
