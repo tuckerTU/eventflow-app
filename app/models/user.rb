4b166f8fafa639c1a2f446f83a30a539759145c8
tuckerTU <tuckerishere@gmail.com>
2021-07-19 14:54:49 -0600

    fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :email, presence: true, uniqueness :true
+    validates :email, presence: true, uniqueness: true
     has_secure_password
     
     has_many :events, through: :guests 
