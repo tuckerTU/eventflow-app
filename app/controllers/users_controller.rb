51aa7bf2a632d929713f0c0acb0464631a48f87b
tuckerTU <tuckerishere@gmail.com>
2021-07-20 10:37:12 -0600

    work on create and user_params methods in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,20 @@ class UsersController < ApplicationController
     end
 
     def create 
-
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to @user
+        else 
+            render :new
+        end
     end
 
     def show 
     end
 
-    #def user_params
-    #    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    #end
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 
 end
